0cd6e01cba59b158f329176ec1e254dd55601c26
yadprab <yadhavanprabhakar744@gmail.com>
2021-09-15 18:06:30 +0530

    final2

--- src/Components/ListComp.tsx
+++ src/Components/ListComp.tsx
@@ -26,29 +26,31 @@ function ListComp() {
       >
         {context?.state.map((s) => {
           return (
-            <AnimateSharedLayout key={s.id}>
-              <motion.li
-                variants={s.isDelete ? deleteAni : Item}
-                className={`todo-tasks ${
-                  s.isCompleted && context.GlobalState.Completed
-                    ? "show--comp"
-                    : !s.isCompleted && context.GlobalState.NotCompleted
-                    ? "show--none"
-                    : "hide--comp"
-                }`}
-                id={s.id}
-              >
-                <div className="top--section">
-                  <p>{s.todo}</p>
-                  <ButtonComp id={s.id} isOpen={s.isOpen} />
-                </div>
-                <Status isCompleted={s.isCompleted} />
-                <AnimatePresence>
-                  {s.isOpen && (
-                    <MidStatus id={s.id} isCompleted={s.isCompleted} />
-                  )}
-                </AnimatePresence>
-              </motion.li>
+            <AnimateSharedLayout key={s.id} type='crossfade'>
+              <AnimatePresence exitBeforeEnter>
+                <motion.li
+                  variants={s.isDelete ? deleteAni : Item}
+                  className={`todo-tasks ${
+                    s.isCompleted && context.GlobalState.Completed
+                      ? "show--comp"
+                      : !s.isCompleted && context.GlobalState.NotCompleted
+                      ? "show--none"
+                      : "hide--comp"
+                  }`}
+                  id={s.id}
+                >
+                  <div className="top--section">
+                    <p>{s.todo}</p>
+                    <ButtonComp id={s.id} isOpen={s.isOpen} />
+                  </div>
+                  <Status isCompleted={s.isCompleted} />
+                  <AnimatePresence>
+                    {s.isOpen && (
+                      <MidStatus id={s.id} isCompleted={s.isCompleted} />
+                    )}
+                  </AnimatePresence>
+                </motion.li>
+              </AnimatePresence>
             </AnimateSharedLayout>
           );
         })}
